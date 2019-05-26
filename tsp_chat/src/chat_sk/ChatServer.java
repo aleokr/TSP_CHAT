9341930d01ef073ffc760d16f315811c8e7d5f8d
aleokr <aleksandra.okrutny1@gmail.com>
2019-05-26 23:36:34 +0200

    26.05 - server's
     changes

--- tsp_chat/src/chat_sk/ChatServer.java
+++ tsp_chat/src/chat_sk/ChatServer.java
@@ -6,9 +6,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 class ChatServer
-{
+{	
+	static String userLogin;
+	static String userPassword;
+	static boolean correct=false;
+	static int number_of_user;
+	static ArrayList <User> list_of_users;
     public static int port = 13000;//port servera
     public static BufferedReader con_br = new BufferedReader(new InputStreamReader(System.in));//czyta  linie nadchodząca
 
@@ -18,19 +24,39 @@ class ChatServer
         System.out.println("server: Waiting for client to connect");
         Socket csock = sock.accept();//odczytuje adres IP ziomka który się z nim łączy
         System.out.println("server: Connection established");
-
-        BufferedReader csock_br = new BufferedReader(new InputStreamReader(csock.getInputStream()));
-        PrintWriter csock_pw = new PrintWriter(csock.getOutputStream(), true);//możliwość wysyłania wiadomości
-
-        Thread chat_server_writer = new ChatWriter("chat_server_writer", csock_pw, con_br);//tworzy nowy watek pisania 
-        chat_server_writer.start();//uruchamia wątek
-
-        String s;
-        while((s = csock_br.readLine()) != null)
-        {
-            System.out.println("\rclient: " + s);//wypisywanie w konsoli rozmowy
-            System.out.print("> ");
+        System.out.println("server: Enter your login ");
+        userLogin=con_br.readLine();
+        System.out.println("server: Enter your password ");
+        userPassword=con_br.readLine();
+        for(int i=0; i<list_of_users.size(); ++i) {
+        	if(list_of_users.get(i).login==userLogin) {
+        		if (list_of_users.get(i).password==userPassword) {
+        			System.out.println("Login and password are correct");
+        			correct=true;
+        			number_of_user=i;
+        		}else {
+        			System.out.println("Login or password are uncorrect");
+        		}
+        	}
         }
+        if(correct==true) {
+        	list_of_users.get(number_of_user).log_in=true;
+        	System.out.println("Enter your friend login");
+        	
+        	BufferedReader csock_br = new BufferedReader(new InputStreamReader(csock.getInputStream()));
+            PrintWriter csock_pw = new PrintWriter(csock.getOutputStream(), true);//możliwość wysyłania wiadomości
+            
+            Thread chat_server_writer = new ChatWriter("chat_server_writer", csock_pw, con_br);//tworzy nowy watek pisania 
+            chat_server_writer.start();//uruchamia wątek
+
+            String s;
+            while((s = csock_br.readLine()) != null)
+            {
+                System.out.println("\rclient: " + s);//wypisywanie w konsoli rozmowy
+                System.out.print("> ");
+            }
+        }
+        
 
         System.out.println("\rserver: Client has disconnected");//stracił połączenie 
         csock.close();//zamyka połaczenie z odpowiednim IP
--- tsp_chat/src/chat_sk/User.java
+++ tsp_chat/src/chat_sk/User.java
@@ -8,11 +8,13 @@ public class User {
 	String IP_adres;
 	int number_of_port;
 	ArrayList <User> list_of_friends;
+	boolean log_in ;
 	
 	public User(String login, String password) {
 		this.login = login;
 		this.password = password;
 		this.list_of_friends = new ArrayList<User>();
+		this.log_in=false;
 	}
 	
 	void addFriend(User friend) {
